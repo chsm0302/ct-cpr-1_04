5c0404ad43c90e392d91d66e2512d0c63009927b
chsm0302 <74017350+chsm0302@users.noreply.github.com>
2022-07-09 01:22:43 +0900

    Add files via upload

---
+++ L1-4_CSM.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+char* solution(const char* phone_number) 
+{
+	int len,i;
+	char* answer;
+	
+	len=strlen(phone_number);
+	answer = (char*)malloc(sizeof(len+1));
+	strcpy(answer,phone_number);
+	
+	for(i=0 ; i<len-4 ; i++)
+	{
+		answer[i]='*';
+	}
+	
+	return answer;
+}
+
+int main(void)
+{
+	char phone_number[20];
+	char* answer;
+	
+	gets(phone_number);
+	answer=solution(phone_number);
+	printf("%s",answer);
+	free(answer);
+}
